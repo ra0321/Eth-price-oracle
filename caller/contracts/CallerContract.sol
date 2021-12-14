234633969a3ad3add8d2d41275474bd632a49dc5
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 20:24:33 -0800

    Lesson 14 Chapter 5

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -7,12 +7,21 @@ contract CallerContract is Ownable {
 
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256 => bool) myRequests;
 
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
+    }
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+
+        emit ReceivedNewRequestIdEvent(id);
     }
 }
