567c2070a52bbf5b772c79ac61824b9ec6ed27f3
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 16:00:29 -0800

    Lesson 14 Chapter 3

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,10 +1,14 @@
 pragma solidity 0.5.0;
 
+import "./EthPriceOracleInterface.sol";
+
 contract CallerContract {
 
+    EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
     }
 }
---
+++ caller/contracts/EthPriceOracleInterface.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.5.0;
+
+contract EthPriceOracleInterface {
+    function getLatestEthPrice() public returns (uint256);
+}
