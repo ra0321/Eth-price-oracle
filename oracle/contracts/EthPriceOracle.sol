7126012d2097ee19cb48fa028d5e3dc2a55c6f71
AlphanetRA <alphanetra0321@gmail.com>
2021-12-14 04:29:24 -0800

    Lesson 16 Chapter 8

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -72,6 +72,12 @@ contract EthPriceOracle {
         uint numResponses = requestIdToResponse[_id].length;
 
         if (numResponses == THRESHOLD) {
+            uint computedEthPrice = 0;
+            for (uint f = 0; f < requestIdToResponse[_id].length; f++) {
+                computedEthPrice += requestIdToResponse[_id][f].ethPrice;
+            }
+            computedEthPrice = computedEthPrice / numResponses;
+
             delete pendingRequests[_id];
 
             CallerContractInterface callerContractInstance;
