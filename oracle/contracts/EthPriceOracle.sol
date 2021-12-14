db6c15469b8fcb6c1855789abc4aa7f9c2b2d29c
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 20:59:12 -0800

    Lesson 14 Chapter 9

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -7,7 +7,7 @@ contract EthPriceOracle is Ownable {
 
     uint private randNonce = 0;
     uint private modulus = 1000;
-    mapping(uint256=>bool) pendingRequests;
+    mapping(uint256 => bool) pendingRequests;
 
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
@@ -15,5 +15,11 @@ contract EthPriceOracle is Ownable {
     function getLatestEthPrice() public returns (uint256) {
         randNonce++;
         uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+
+        pendingRequests[id] = true;
+
+        emit GetLatestEthPriceEvent(msg.sender, id);
+
+        return id;
     }
 }
