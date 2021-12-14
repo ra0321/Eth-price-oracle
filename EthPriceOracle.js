c3772b0f3e1d335d7200f5435ef85976ebce4545
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 21:55:43 -0800

    Lesson 15 Chapter 2

--- EthPriceOracle.js
+++ EthPriceOracle.js
@@ -8,7 +8,21 @@ const MAX_RETRIES = process.env.MAX_RETRIES || 5
 const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json')
 var pendingRequests = []
 
-async function getOracleContract(web3js) {
+async function getOracleContract (web3js) {
 	const networkId = await web3js.eth.net.getId()
 	return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
+}
+
+async function filterEvents (oracleContract, web3js) {
+	oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
+		if (err) {
+			console.error('Error on event', err)
+			return
+		}
+		await addRequestToQueue(event)
+	})
+
+	oracleContract.events.SetLatestEthPriceEvent(async (err, event) => {
+		if (err) console.error('Error on event', err)
+	})
 }
