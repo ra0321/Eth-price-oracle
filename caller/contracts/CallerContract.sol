64a1dc05f12d020fae9174594f6a3af7d04262a0
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 16:07:58 -0800

    Lesson 14 Chapter 4

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,14 +1,18 @@
 pragma solidity 0.5.0;
 
 import "./EthPriceOracleInterface.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract CallerContract {
+contract CallerContract is Ownable {
 
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
 
-    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+    event newOracleAddressEvent(address oracleAddress);
+
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
+        emit newOracleAddressEvent(oracleAddress);
     }
 }
