a7668177891cab15a765a55d69844e7afd758d9d
AlphanetRA <alphanetra0321@gmail.com>
2021-12-14 03:01:45 -0800

    Lesson 16 Chapter 1

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -1,10 +1,14 @@
 pragma solidity 0.5.0;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/access/Roles.sol";
 import "./CallerContractInterface.sol";
 
-contract EthPriceOracle is Ownable {
+contract EthPriceOracle {
 
+    using Roles for Roles.Role;
+
+    Roles.Role private owners;
+    Roles.Role private oracles;
     uint private randNonce = 0;
     uint private modulus = 1000;
     mapping(uint256 => bool) pendingRequests;
