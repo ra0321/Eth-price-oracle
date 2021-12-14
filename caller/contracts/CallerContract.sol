fec37d8c515d8e68a4ed38ce6eab731df217caae
AlphanetRA <alphanetra0321@gmail.com>
2021-12-13 20:37:02 -0800

    Lesson 14 Chapter 6

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -5,12 +5,14 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract CallerContract is Ownable {
 
+    uint256 private ethPrice;
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
     mapping(uint256 => bool) myRequests;
 
     event newOracleAddressEvent(address oracleAddress);
     event ReceivedNewRequestIdEvent(uint256 id);
+    event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
@@ -23,5 +25,13 @@ contract CallerContract is Ownable {
         myRequests[id] = true;
 
         emit ReceivedNewRequestIdEvent(id);
+    }
+
+    function callback(uint256 _ethPrice, uint256 _id) public {
+        require(myRequests[_id], "This request is not in my pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
+
+        emit PriceUpdatedEvent(_ethPrice, _id);
     }
 }
